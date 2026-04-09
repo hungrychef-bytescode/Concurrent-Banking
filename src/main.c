766740659d06deac63babfb671585a4088a20ce2
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-04-09 22:04:51 +0800

    feat: added CLI check for required argument count

--- src/main.c
+++ src/main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("argc: %d", argc);
+
+    if (argc < 5) {
+        printf("USE: ./bankdb --accounts= --trace= --deadlock= --tick-ms --verbose");
+        return 1;
+    }
+    
+    printf("argc: %d\n", argc);
 
     for (int i = 0; i < argc; i++) {
         printf("Argument %d: %s\n", i, argv[i]);
     }
+
+
 
     return 0;
 }
