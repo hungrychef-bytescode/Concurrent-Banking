cd9dc8ce91e7c354dcf00b0910f278eefbbfdbb2
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-04-14 00:19:31 +0800

    feat: handle CLI input and print parsed arguments for checking and debugging

--- src/main.c
+++ src/main.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
+#include "cli_parser.h"
 
 int main(int argc, char *argv[]) {
 
-    if (argc < 5) {
+    //move this to cli parser func
+    if (argc < 4) {
         printf("USE: ./bankdb --accounts= --trace= --deadlock= --tick-ms --verbose");
         return 1;
     }
-    
+
     printf("argc: %d\n", argc);
 
     for (int i = 0; i < argc; i++) {
         printf("Argument %d: %s\n", i, argv[i]);
     }
 
+    CLIParse bank_command = parse_cli(argc, argv);
 
+    printf("test\n");
+    printf("account: %s\n", bank_command.accounts);
+    printf("trace: %s\n", bank_command.trace);
+    printf("deadlock: %s\n", bank_command.deadlock_mode);
+    printf("tick: %d\n", bank_command.tick);
+    printf("verbose: %d\n", bank_command.verbose);
 
     return 0;
 }
