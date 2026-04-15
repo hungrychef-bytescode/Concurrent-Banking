f6fd42a4e8731db564bb9797eda24baf536e7b0d
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-04-15 12:03:44 +0800

    feat: integrate accounts parser and print initial balances

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "cli_parser.h"
+#include "accounts_parser.h"
 
 int main(int argc, char *argv[]) {
 
@@ -9,14 +10,26 @@ int main(int argc, char *argv[]) {
         printf("Argument %d: %s\n", i, argv[i]);
     }
 
-    CLIParse bank_command = parse_cli(argc, argv);
+    CLIParse cli = parse_cli(argc, argv);
 
     printf("test\n");
-    printf("account: %s\n", bank_command.accounts);
-    printf("trace: %s\n", bank_command.trace);
-    printf("deadlock: %s\n", bank_command.deadlock_mode);
-    printf("tick: %d\n", bank_command.tick);
-    printf("verbose: %d\n", bank_command.verbose);
+    printf("account: %s\n", cli.accounts);
+    printf("trace: %s\n", cli.trace);
+    printf("deadlock: %s\n", cli.deadlock_mode);
+    printf("tick: %d\n", cli.tick);
+    printf("verbose: %d\n", cli.verbose);
 
+    Account accounts[MAX_ACCOUNTS];
+
+    int count = parse_accounts(cli.accounts, accounts, MAX_ACCOUNTS);
+    printf("count: %d\n", count);
+    printf("Initial Account Balance\n");
+
+    for (int i = 0; i < count; i++) {
+        printf("account id: %d\n", accounts[i].account_id);
+        printf("balance in centavos: %d\n", accounts[i].balance_centavos);
+    }
+
+   
     return 0;
 }
