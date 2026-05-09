c6ecc2f2e1b8bc3bd7605a9d71854f344252b0ab
Angel May <167489251+janiolangel@users.noreply.github.com>
2026-05-09 20:51:13 +0800

    feat: transaction.c

---
+++ src/transaction.c
@@ -0,0 +1,47 @@
+#ifndef TRANSACTIONS_H
+#define TRANSACTIONS_H
+
+#include <pthread.h>
+
+#define MAX_OP_PER_TX    256
+#define MAX_TRANSACTIONS 100
+
+typedef enum {
+    OP_DEPOSIT,   // Add money to account
+    OP_WITHDRAW,  // Remove money from account
+    OP_TRANSFER,  // Move money between two accounts
+    OP_BALANCE,   // Read account balance
+} OpType;
+
+typedef struct {
+    OpType type;
+    int account_id;       // Primary account
+    int amount_centavos;  // Amount in centavos
+    int target_account;   // For TRANSFER only
+} Operation;
+
+typedef enum {
+    TX_RUNNING,
+    TX_COMMITTED,
+    TX_ABORTED
+} TxStatus;
+
+typedef struct {
+    char       tx_id[32];
+    Operation  ops[MAX_OP_PER_TX];
+    int        num_ops;
+    int        start_tick;    // When transaction should start (from trace file)
+    pthread_t  thread;        // The pthread handle
+
+    // Timing (measured in ticks)
+    int        actual_start;
+    int        actual_end;
+    int        wait_ticks;
+
+    // Status
+    TxStatus   status;
+} Transaction;
+
+int parse_transactions(const char *filename, Transaction *transactions, int max_transactions);
+
+#endif
